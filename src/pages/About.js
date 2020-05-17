bdc6ff83b2f82638da1b8d6656eb4b34ccd3f0a6
Satish Fulwani <satish.fulwani63@gmail.com>
2020-05-17 17:54:04 +0530

    Add Hero to Home

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia ab
+        soluta itaque culpa qui neque cupiditate quae a. Incidunt omnis ducimus
+        recusandae laborum! Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Eligendi facere expedita iusto ut sint aliquid dolorem repellendus
+        natus perspiciatis asperiores id corrupti autem, tempora voluptates fuga
+        dolorum nemo pariatur, quasi aperiam aspernatur.
+      </p>
+    </section>
+  );
 }
